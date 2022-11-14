8687c603a786f01143aa551a583455ffcecf4eee
anuragkulkarni20 <68266521+anuragkulkarni20@users.noreply.github.com>
2022-11-14 13:00:25 +0530

    Add files via upload

---
+++ Basics java prgrm/src/com/basic/interview/prgrms/nullvsmpty.java
@@ -0,0 +1,29 @@
+package com.basic.interview.prgrms;
+
+public class nullvsmpty {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		String s1 = null;
+		
+		String s2 ="";
+		
+		
+		System.out.println(s1.length());
+		System.out.println(s2.length());
+		
+		//difference between null and empty 
+		
+		/*NULL = When String variable is assigned with null, 
+		 * it indicates that the variable is not actually referring 
+		 * to any memory location in the heap.
+
+        STRING When a String variable is assigned with an empty String,
+         it indicates that the reference variable is referring to 
+         a memory location of a string with zero length.*/
+		
+	}
+
+}
